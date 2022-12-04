e7f81918bc3fd6996ec6c5de62a5864f349feed6
S. M. Redwan <smredwan.info@gmail.com>
2022-12-04 23:05:19 +0600

    Solved bug

--- third_sem/assignment_1/one.c
+++ third_sem/assignment_1/one.c
@@ -4,36 +4,37 @@
 #include <stdlib.h>
 
 int main() {
+    int *arr, *arr2;
     int i, n, m;
     
     printf("Enter the size of primary size of your array: ");
     scanf("%d", &n);
-    int arr[n];
+    arr = (int *)malloc(sizeof(int)*n);
     m = n+2;
-    int arr2[2];
+    arr2 = (int *)malloc(sizeof(int)*m);
 
     printf("Enter element of your array:\n");
     for(i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%d", arr+i);
     }
 
     printf("Before insert your elements of your array are:\n\t");
     for(i = 0; i < n; i++) {
-        printf("%d  ", arr[i]);
+        printf("%d  ", *(arr+i));
     }
 
     for(i = 0 ; i < n ; i++){
-        arr2[i] = arr[i];
+        *(arr2 + i) = *(arr + i);
     }
 
-    for(i = n; i < m+1; i++) {
-        printf("\nEnter a element for isert last: ");
-        scanf("%d", &arr2[i]);
+    for(i = n; i < m; i++) {
+        printf("\nEnter a element for insert last: ");
+        scanf("%d", arr2+i);
     }
 
     printf("\nAfter insert two element at last you array is:\n\t");
-    for(i = 0; i < m+1; i++) {
-        printf("%d  ", arr2[i]);
+    for(i = 0; i < m; i++) {
+        printf("%d  ", *(arr2 + i));
     }
 
     return 0;
